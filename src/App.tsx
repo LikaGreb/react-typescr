22d22fbb3fa7dca8aea7d084fcd31d2a8402bb78
LikaGreb <114307021+LikaGreb@users.noreply.github.com>
2022-12-10 20:33:24 +0200

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import img1 from './components/images/1.jpg';
+import img2 from './components/images/2.jpg';
+import img3 from './components/images/3.jpg';
+import img4 from './components/images/4.jpg';
 import './App.css';
+import { Button } from 'react-bootstrap';
 
 function App() {
+  const [count, setCount] = useState<number>(0);
+  const [pic, setPic] = useState<any>(img1);
+
+  function counter() {
+    setCount(count + 1);
+    console.log(count);
+
+    switch (count % 4) {
+      case (0): setPic(img2);
+        break;
+      case (1): setPic(img3);
+        break;
+      case (2): setPic(img4);
+        break;
+      case (3): setPic(img1);
+        break;
+      default:
+        break;
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={pic} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <Button className="btn" variant="info" onClick={counter}>Next</Button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
